a4305129debfb0d92933d18248fe999892695f7e
Foster Stule <camodude5@yahoo.com>
2018-05-30 15:12:08 -0400

    inital commit

--- PigDice/Program.cs
+++ PigDice/Program.cs
@@ -10,6 +10,17 @@ namespace PigDice
 	{
 		static void Main(string[] args)
 		{
+			Random rnd = new Random();
+			bool kick = false;
+			int total = 0;
+			while (kick == false) {
+				int temp = rnd.Next(1, 7);
+				total += temp;
+				Console.WriteLine(temp);
+				if (temp == 1) 
+				{ kick = true; }
+			}
+			Console.WriteLine("your score is " + total);
 		}
 	}
 }
